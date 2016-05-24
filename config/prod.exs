215d146c738c55a8f9451b630b120e8161179b7c
Marvel Mathew <marvel@busbud.com>
2016-05-23 23:03:22 -0400

    Pull `prod.secret.exs` from the right directory

--- config/prod.exs
+++ config/prod.exs
@@ -60,4 +60,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "data/prod.secret.exs"
+import_config "../data/prod.secret.exs"
